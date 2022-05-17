561a99462b47c0223820667e6a430ed516040a20
vncsvgr <vinicius.gabriel83@yahoo.com>
2022-05-16 22:27:26 -0300

    Adicionar arquivos de projeto.

---
+++ DolarJson/DolarJson/Dolar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DolarJson
+{
+    public class Dolar
+    {
+        public string bid { get; set; }
+        public string ask { get; set; }
+        public string varBid { get; set; }
+        public string pctChange { get; set; }
+        public string high { get; set; }
+        public string low { get; set; }
+    }
+}
---
+++ DolarJson/DolarJson/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace DolarJson
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void btnPesquisa_Clicked(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+            var json = await client.GetStringAsync($"https://economia.awesomeapi.com.br/json/last/USD-BRL");
+            var dados = JsonConvert.DeserializeObject<Dolar>(json);
+
+            edtCompra.Text = dados.bid;
+            edtVenda.Text = dados.ask;
+            edtMax.Text = dados.high;
+            edtMin.Text = dados.low;
+            edtPctVariacao.Text = dados.pctChange;
+            edtVariacao.Text = dados.varBid;
+        }
+    }
+}
